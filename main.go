9545111e37720c93e3f79990aefc425d44f13a80
artisan <iisakov.artisan@yandex.ru>
2024-05-03 18:37:46 +0300

    Тестовый скрипт Changes to be committed: 	new file:   go.mod 	new file:   go.sum 	new file:   main.go

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"os"
+
+	"github.com/nfnt/resize"
+)
+
+func main() {
+	var maxWidth uint = 1200
+	var maxHight uint = 900
+	var newWidth, newHight uint
+
+	imgFile, err := os.Open("test.jpg")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	newImgFile, err := os.Create("result.jpg")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	in := bufio.NewReader(imgFile)
+	out := bufio.NewWriter(newImgFile)
+
+	img, _, err := image.Decode(in)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	bounds := img.Bounds()
+
+	fmt.Println(bounds.Size())
+
+	if uint(bounds.Size().X) > maxWidth {
+		fmt.Println("Ширина изображения больше максимальной")
+
+		newHight = uint(float32(maxWidth) / float32(bounds.Size().X) * float32(bounds.Size().Y))
+		newWidth = maxWidth
+		fmt.Printf("Новая размер изображения: %d %d \n", newWidth, newHight)
+
+		if newHight > maxHight {
+			fmt.Println("Высота изображения всё ещё больше максимальной")
+			newWidth = uint(float32(bounds.Size().X) * float32(maxWidth) / float32(bounds.Size().Y))
+			newHight = maxHight
+			fmt.Printf("Новая размер изображения: %d %d \n", newWidth, newHight)
+		}
+	}
+
+	newImg := resize.Resize(newWidth, newHight, img, resize.Lanczos3)
+
+	// Encode uses a Writer, use a Buffer if you need the raw []byte
+	err = jpeg.Encode(out, newImg, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	// check err
+}
